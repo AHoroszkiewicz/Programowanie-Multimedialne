d65f2fa4e7e5db31de73047344d400c47157bd0d
Adam Horo <loginadas@gmail.com>
2025-03-30 09:50:21 +0200

    t3z3

--- Temat 3/Zadanie 3/v_shader.glsl
+++ Temat 3/Zadanie 3/v_shader.glsl
@@ -7,8 +7,14 @@ uniform mat4 M;
 
 //Atrybuty
 in vec4 vertex; //wspolrzedne wierzcholka w przestrzeni modelu
+in vec4 color;
+
+out vec4 vertex_color;
 
 
 void main(void) {
-    gl_Position=P*V*M*vertex;
+	gl_Position = P * V * M * vertex;
+	float dist = distance(V * M * vertex, vec4(0.0, 0.0, 0.0, 1.0));
+	float normalizedDistance = clamp((dist - 3.3) / (5.0 - 3.3), 0.0, 1.0);
+	vertex_color = color * (1.0 - normalizedDistance);
 }

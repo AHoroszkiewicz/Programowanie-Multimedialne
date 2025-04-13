eeef15e9ece28963c0296e8a0e41016da496aa04
Adam Horo <loginadas@gmail.com>
2025-04-13 09:09:34 +0200

    t4z4

--- Temat 4/Zadanie 4/v_shader.glsl
+++ Temat 4/Zadanie 4/v_shader.glsl
@@ -1,19 +1,26 @@
 #version 330
 
-//Zmienne jednorodne
+// Uniformy
 uniform mat4 P;
 uniform mat4 V;
 uniform mat4 M;
 
-//Atrybuty
-in vec4 vertex; //wspolrzedne wierzcholka w przestrzeni modelu
-in vec4 normal; //wektor normalny w przestrzeni modelu
-in vec4 color; //kolor skojarzony z wierzcho³kiem
-in vec2 texCoord; //wspó³rzêdna teksturowana
+const vec4 lightPos = vec4(0.0, 0.0, -6.0, 1.0); // Pozycja œwiat³a w przestrzeni œwiata
 
-out vec4 i_c;
+// Atrybuty
+in vec4 vertex;
+in vec3 normal;
+
+// Wyjœcie do fragment shadera
+out vec3 fragPos;  // Przesy³amy pozycjê wierzcho³ka
+out vec3 normalDir; // Normalna wektorowa
+out vec4 lightPosEye; // Pozycja œwiat³a w przestrzeni kamery
 
 void main(void) {
-    i_c=color;
-    gl_Position=P*V*M*vertex;
+    vec4 vertexEye = V * M * vertex; // Wierzcho³ek w przestrzeni kamery
+    fragPos = vertexEye.xyz; // Przesy³amy pozycjê w przestrzeni kamery
+    normalDir = normalize((V * M * vec4(normal, 0.0)).xyz); // Normalna w przestrzeni kamery
+    lightPosEye = V * lightPos; // Pozycja œwiat³a w przestrzeni kamery
+
+    gl_Position = P * vertexEye; // Ustalanie ostatecznej pozycji wierzcho³ka
 }

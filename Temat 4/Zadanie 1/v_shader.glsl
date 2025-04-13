9a17cfd93919d89da312ee71a54d7c85f2bf1fad
Adam Horo <loginadas@gmail.com>
2025-04-13 08:50:00 +0200

    t4z1

--- Temat 4/Zadanie 1/v_shader.glsl
+++ Temat 4/Zadanie 1/v_shader.glsl
@@ -5,15 +5,32 @@ uniform mat4 P;
 uniform mat4 V;
 uniform mat4 M;
 
+uniform vec4 lightPos;
 //Atrybuty
 in vec4 vertex; //wspolrzedne wierzcholka w przestrzeni modelu
-in vec4 normal; //wektor normalny w przestrzeni modelu
-in vec4 color; //kolor skojarzony z wierzcho³kiem
-in vec2 texCoord; //wspó³rzêdna teksturowana
+in vec3 normal; //wspolrzedne normalnej wierzcholka w przestrzeni modelu
+in vec4 color;
 
-out vec4 i_c;
+out vec4 vertex_color;
+
+
 
 void main(void) {
-    i_c=color;
-    gl_Position=P*V*M*vertex;
+    // Transformacja pozycji wierzcho³ka i normalnych do przestrzeni oka
+    vec4 vertex_eye = V * M * vertex;
+    vec3 normal_eye = normalize(mat3(V * M) * normal); // mat3 usuwa translacjê
+
+    // Transformacja pozycji œwiat³a
+    vec4 light_eye = V * lightPos;
+    vec3 l = normalize(light_eye.xyz - vertex_eye.xyz);
+
+    // K¹t miêdzy œwiat³em a normaln¹
+    float NdotL = max(dot(normal_eye, l), 0.0);
+
+    // Bia³e œwiat³o rozproszone * NdotL, bez œwiat³a otoczenia
+    vec3 diffuse = vec3(1.0, 1.0, 1.0) * NdotL;
+
+    vertex_color = vec4(diffuse, 1.0);
+
+    gl_Position = P * vertex_eye;
 }

f869f5d60356b69f0b3b69a30b2740a8fd029fff
Adam Horo <loginadas@gmail.com>
2025-05-25 08:56:05 +0200

    t5z4

--- Temat 5/Zadanie 4/Program.cs
+++ Temat 5/Zadanie 4/Program.cs
@@ -29,16 +29,79 @@ namespace PMLabs
 
         public static void KeyProcessor(System.IntPtr window, Keys key, int scanCode, InputState state, ModifierKeys mods)
         {
+            if (key == Keys.Space && state == InputState.Press)
+            {
+                int sourcestate;
+                AL.GetSource(source, ALGetSourcei.SourceState, out sourcestate);
+
+                if ((ALSourceState)sourcestate == ALSourceState.Playing)
+                {
+                    AL.SourcePause(source);
+                }
+                else if ((ALSourceState)sourcestate == ALSourceState.Paused)
+                {
+                    AL.SourcePlay(source);
+                }
+            }
+        }
+
+        public static short signal(double t, double f, double A)
+        {
+            double value = 4 * A * Math.Abs(t * f - Math.Floor(t * f + 0.5)) - A;
+            return (short)value;
         }
 
         public static void InitSound()
         {
+            device = ALC.OpenDevice(null);
+            context = ALC.CreateContext(device, new ALContextAttributes());
+            ALC.MakeContextCurrent(context);
 
+            buf = AL.GenBuffer();
+
+            double f = 500;
+            double A = short.MaxValue / 10;
+            int fp = 44100;
+            double op = 1.0 / fp;
+            int lp = 10 * fp;
+            short[] data = new short[lp];
+            for (int x = 0; x < lp; x++)
+            {
+                data[x] = signal(op * x, f, A); //generuj kolejne próbki
+            }
+            AL.BufferData<short>(buf, ALFormat.Mono16, data, fp);
+
+            //int channels, bits, sampleFreq;
+            //byte[] data = Helpers.LoadWave("misc_sound.wav", out channels, out bits, out sampleFreq);
+            //AL.BufferData<byte>(buf, Helpers.GetFormat(channels, bits), data, sampleFreq);
+
+            source = AL.GenSource();
+
+            AL.BindBufferToSource(source, buf);
+
+            AL.SourcePlay(source);
+
+            AL.Source(source, ALSourceb.Looping, true);
         }
 
         public static void FreeSound()
         {
+            AL.SourceStop(source);
+            AL.DeleteSource(source);
+            AL.DeleteBuffer(buf);
 
+            if (context != ALContext.Null)
+            {
+                ALC.MakeContextCurrent(ALContext.Null);
+                ALC.DestroyContext(context);
+            }
+            context = ALContext.Null;
+
+            if (device != ALDevice.Null)
+            {
+                ALC.CloseDevice(device);
+            }
+            device = ALDevice.Null;
         }
 
         public static void SoundEvents()
